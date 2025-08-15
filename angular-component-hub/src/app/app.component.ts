0c77b86a5081338565a02f21f95e3ba36b4aafec
Jean Soares <jeansoaresdeveloper@gmail.com>
2025-08-14 22:26:21 -0300

    feat: criando esqueleto de listagem

--- angular-component-hub/src/app/app.component.ts
+++ angular-component-hub/src/app/app.component.ts
@@ -1,12 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { EntityService } from '../entity/entity.service';
+import { ListComponent } from './shared/list/list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ListComponent],
+  providers: [
+    EntityService,
+    HttpClient,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-component-hub';
+
+  constructor(
+    protected readonly entityService: EntityService<any, any>
+  ) {}
 }
