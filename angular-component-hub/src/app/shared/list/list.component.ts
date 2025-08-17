bee5cea35715fdde60496adee8979a089dbd65ee
Jean Soares <jeansoaresdeveloper@gmail.com>
2025-08-17 12:39:38 -0300

    feat: criando componente de pagesize

--- angular-component-hub/src/app/shared/list/list.component.ts
+++ angular-component-hub/src/app/shared/list/list.component.ts
@@ -1,18 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
 import { Pageable } from '../../model/pageable.model';
+import { PageSizeComponent } from "../page-size/page-size.component";
 import { PaginatorComponent } from '../paginator/paginator.component';
 import { ListParams } from './params/list-params.model';
 
 @Component({
   selector: 'app-list',
-  imports: [CommonModule, PaginatorComponent],
+  imports: [CommonModule, PaginatorComponent, PageSizeComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
 export class ListComponent implements OnInit {
 
   @Input({ required: true }) params!: ListParams;
+  @Input() quantityPages: number[] = [20, 5, 10, 50, 100];
 
   responseData: Pageable = { content: [], empty: false, first: true, last: true, number: 0, totalPages: 30 } as any;
 
@@ -22,6 +24,10 @@ export class ListComponent implements OnInit {
       this.responseData.content.push(`teste ${i}`);
     }
 
+  }
+
+  detectChangePageSize(value: number) {
+    console.log('Mudou size fazer o findAll Com tamanho diferente:' + value);
   }
 
 }
---
+++ angular-component-hub/src/app/shared/page-size/page-size.component.ts
@@ -0,0 +1,44 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-page-size',
+  imports: [CommonModule],
+  template: `
+    <select [value]="selectedPageSize()" (change)="onChangePageSize($event)">
+      @for (quantity of quantityPages; track quantity) {
+        <option [value]="quantity">{{ quantity }}</option>
+      }
+    </select>
+  `,
+  styles: `
+    select {
+      padding: 5px 10px;
+      background: none;
+      border: 1px solid #ccc;
+      outline: none;
+    }
+  `
+})
+export class PageSizeComponent implements OnInit {
+
+  @Input({ required: true }) quantityPages!: number[];
+
+  @Output() detectPageSize: EventEmitter<number> = new EventEmitter();
+
+  readonly selectedPageSize = signal(20);
+
+  ngOnInit(): void {
+    this.selectedPageSize.set(this.quantityPages[0]);
+  }
+
+  onChangePageSize(event: Event) {
+
+    const value = (event.target as HTMLSelectElement).value;
+    if (!value) return;
+
+    this.selectedPageSize.set(parseInt(value));
+    this.detectPageSize.emit(this.selectedPageSize());
+  }
+
+}
