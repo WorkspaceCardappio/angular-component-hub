13d86e2c68244aaf7cca6e98b6046d446cc5f8a7
joaoagr1 <joaorolo.dev@gmail.com>
2025-08-18 21:00:37 -0300

    feat: começando component auto complete

---
+++ angular-component-hub/src/app/shared/auto-complete/auto-complete.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Size } from '../../utils/size.model';
+import { ButtonParams } from '../params/button-params.model';
+
+@Component({
+  selector: 'app-generic-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './auto-complete.component.html',
+  styleUrls: ['./auto-complete.scss']
+})
+export class AutoCompleteComponent {
+
+
+  }
+
+
+
+}
