677f52383dfb8c4b04d523e4f82fb7269a987e8d
joaoagr1 <joaorolo.dev@gmail.com>
2025-08-29 18:16:07 -0300

    refactor: resolvendo comentários (top label dinamico + data default)

--- angular-component-hub/src/app/shared/date-picker/date-picker.component.ts
+++ angular-component-hub/src/app/shared/date-picker/date-picker.component.ts
@@ -2,23 +2,25 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MAT_DATE_LOCALE, MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
 
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'date-picker',
   templateUrl: 'date-picker.component.html',
-  providers: [provideNativeDateAdapter()],
+  providers: [provideNativeDateAdapter(),provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }],
   imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, FormsModule, MatNativeDateModule],
 })
 export class DatePickerComponent {
 
   DEFAULT_SIZE: string = "30";
 
-  @Input() selectedDate: Date | null = null;
+  @Input() selectedDate: Date | null = new Date()
   @Output() dateChange = new EventEmitter<Date | null>();
   @Input() size: string = this.DEFAULT_SIZE;
+  @Input() topLabel: string = "Data";
 
   onDateChange(event: any) {
     this.dateChange.emit(event.value);
