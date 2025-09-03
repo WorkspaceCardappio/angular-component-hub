5fd10b6452c24b65a67118228f6660ed26e1c98e
LorenzoMBegnozzi <lorenzobegnozzi@hotmail.com>
2025-09-03 00:46:02 -0300

    refactor: Removendo comentarios

--- angular-component-hub/src/app/shared/upload-image/upload-image.component.ts
+++ angular-component-hub/src/app/shared/upload-image/upload-image.component.ts
@@ -2,52 +2,50 @@ import { Component, ViewChild, ElementRef, ChangeDetectorRef, Input } from '@ang
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-image-upload', //nome do componente
-  standalone: true, //indica que é um componente independente
+  selector: 'app-image-upload',
+  standalone: true, 
   imports: [CommonModule],
   styleUrls: ['./upload-image.component.scss'],
   templateUrl: './upload-image.component.html',
 })
 export class ImageUploadComponent {
-  imagePreview: string | ArrayBuffer | null = null; //imagem pode ser string, array de bytes ou null (antes de carregar algo)
-  fileName: string = ''; //guarda o nome do arquivo
+  imagePreview: string | ArrayBuffer | null = null; 
+  fileName: string = ''; 
 
   @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
   HIGHLIGHT_COLOR: string | undefined;
   PRIMARY_COLOR_TEXT: string | undefined;
-  constructor(private cdr: ChangeDetectorRef) { } //para forçar atualização do preview rapida
+  constructor(private cdr: ChangeDetectorRef) { } 
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
-    if (!input.files?.length) return; //se n tiver arquivo, sai
+    if (!input.files?.length) return; 
 
-    const file = input.files[0]; //pega o primeiro arquivo (só permite um)
+    const file = input.files[0]; 
 
-    if (!file.type.startsWith('image/')) { //verifica se é imagem
-      alert('Por favor, selecione apenas imagens.'); //alerta se n for
-      input.value = ''; //reseta o input
-      return; //sai da função
+    if (!file.type.startsWith('image/')) { 
+      alert('Por favor, selecione apenas imagens.');
+      input.value = ''; 
+      return; 
     }
 
-    this.fileName = file.name; //guarda o nome do arquivo
+    this.fileName = file.name;
 
-    //pega a imagem, converte para string que o navegador entende como imagem, e guarda em imagePreview
-    //api nativa js para ler arquivos 
     const reader = new FileReader();
     reader.onload = () => {
-      this.imagePreview = reader.result; // garante atualização do preview
+      this.imagePreview = reader.result; 
       this.cdr.detectChanges();
     };
     reader.readAsDataURL(file);
   }
-  // remove a imagem e o nome do arquivo
+
   removeImage(): void {
     this.imagePreview = null;
     this.fileName = '';
 
     const fileInput = document.querySelector<HTMLInputElement>('input[type="file"]');
     if (fileInput) {
-      fileInput.value = ''; // reseta input
+      fileInput.value = ''; 
     }
   }
 }
