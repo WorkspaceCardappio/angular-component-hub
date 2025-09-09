788ea77da1bb770b84dd31bb3cb810e747730a36
joaoagr1 <joaorolo.dev@gmail.com>
2025-09-09 12:03:36 -0300

    refactor: removendo import para pipe

--- angular-component-hub/src/app/app.component.ts
+++ angular-component-hub/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import {HeaderComponent} from '../../projects/component-hub/src/lib/header/header.component';
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
