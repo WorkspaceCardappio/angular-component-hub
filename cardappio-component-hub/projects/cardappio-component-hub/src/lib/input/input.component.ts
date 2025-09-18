f9bfbd69c9f1279d3ebefd9502d31f46055afd37
jean.soares <jean.soares@elotech.com.br>
2025-09-18 20:14:52 -0300

    fix: permitindo passar formControlName

--- cardappio-component-hub/projects/cardappio-component-hub/src/lib/input/input.component.ts
+++ cardappio-component-hub/projects/cardappio-component-hub/src/lib/input/input.component.ts
@@ -1,26 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'cardappio-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.scss'],
-  standalone: true,
-
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent {
+export class InputComponent implements ControlValueAccessor {
 
-  @Input() isDisabled: boolean = false;
-  @Input() placeholder: string = 'Label do Input';
-  @Input() label: string = 'Label superior';
-  width: number = 50;
-  height: number = 3;
+  @Input() placeholder: string = '';
+  @Input() label: string = '';
   @Input() fontSize: number = 16;
-  @Input() public value: string = '';
+  @Input() width: number = 15;
+  @Input() height: number = 20;
+  @Input() type: string = 'text';
+  @Input() disabled = false;
 
-  @Output() public inputValue = new EventEmitter<string>();
+  public value: string = '';
+
+  private onChange = (value: any) => {};
+  private onTouched = () => {};
+
+  onBlur() {
+    this.onTouched();
+  }
+
+  writeValue(value: string): void {
+    this.value = value ?? '';
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 
   onValueChange(newValue: string): void {
     this.value = newValue;
-    this.inputValue.emit(this.value);
+    this.onChange(newValue);
+    this.onTouched();
   }
 }
